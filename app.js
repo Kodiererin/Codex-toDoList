f19f2524d4b8bc0bca62706a221cdc9293f90e5f
Kodiererin <iamujjwal45@gmail.com>
2023-02-11 16:50:33 +0530

    Adding Files

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ async function main() {
   await mongoose.connect('mongodb://127.0.0.1:27017/toDoList',{useNewUrlParser : true});  
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
-mongoose.set('strictQuery', true);
+mongoose.set('strictQuery', false);
 
 const itemsSchema = mongoose.Schema({
     name : {
@@ -26,10 +26,32 @@ const itemsSchema = mongoose.Schema({
     }
 });
 
-const itemsModel = mongoose.model('itemsModel',itemsSchema);
+const Item = new mongoose.model('Item',itemsSchema);
+
+const item1 = new Item({
+    name : "Welcome to the toDo List"
+})
+
+const item2 = new Item({
+    name : "Hit the + button to add a New Item"
+})
+
+const item3 = new Item({
+    name : "<-- Hit this to delete an Item."
+})
+
+const defaultItems = [item1 , item2 , item3];
+
+const listSchema = {
+    name : String,
+    items : [itemsSchema],
+};
+
+const List = new mongoose.model("List" , listSchema);
+
+const itemsModel = mongoose.model('itemsModel',itemsSchema); 
 // Ise Mt Dekhna Ye By Default List h------------------------>
 app.get('/', async function(req,res){
-    try{
         const listItem = await itemsModel.find();       // Async and await is important.
         console.log(listItem)
         let agenda = "Today";
@@ -41,34 +63,69 @@ app.get('/', async function(req,res){
         else{
          res.render('list',{agenda,listItem});
        }
-    }catch(err){
         // console.log(err)
         console.log('Error Found '+err);
-    }
 })
+
+// customId is the ID which I am getting the Parameter.
+
+app.get('/:customId' ,async function(req,res){
+    const customId = req.params.customId;
+    console.log("Jumped Here");
+    List.findOne({ name: customId }, await function (err, found) {
+        if (!err) {
+            if (!found) {
+                console.log("Not Found");
+                const list = new List({
+                    name : customId,
+                    items : defaultItems,
+                });
+                list.save();
+                res.redirect("/"+customId)
+            } else {
+                console.log(found);
+
+                res.render("list",{agenda : found.name , listItem : found.items})
+            }
+        } else {
+            console.log(err);
+
+        }
+    })
+    
+});
 
 app.post('/',async function (req,res){
     console.log(req.body);
-    let myTask = req.body.newItem+"";
-    let p = new itemsModel({
-        name : req.body.newItem
-    })
-   if(myTask.length>0){
-    p.save(function(err){
-        if(err){
-            console.log(err);
-        }
-    })
-   }
-    const listItem = await itemsModel.find();       // Async and await is important.
-            let agenda = "Today";
-            if(listItem.length==0){
-                res.render('list',{agenda,listItem:["Please Add An Item"]});
-            }
-            else{
-             res.render('list',{agenda,listItem});
-           }
-    });
+    const itemName = req.body.newItem;
+    const listValue = req.body.list;
+    
+    console.log(itemName);
+    console.log(listValue);
+  
+    const item = new Item({
+        name: itemName
+      });
+    
+    if(listValue === 'Today')
+    {   
+        item.save();
+        res.redirect('/');
+    }
+    else{
+        console.log("The Data is Getting Entered Here");
+        List.findOne({name : listValue } , function(err,foundList){
+            foundList.items.push(item);
+            console.log(foundList);
+            foundList.save();
+            res.redirect('/'+listValue);
+        })
+    }   
+});
+
+
+
+
 app.post('/delete',async function(req,res){
   console.log('Deleting the Task'); 
   const data = req.body.check;
